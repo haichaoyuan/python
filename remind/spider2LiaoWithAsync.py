7b7ff5a9f3f13fa9c1ce8fee5161b8b69d047b4e
Administrator <944951213@qq.com>
2017-05-18 11:09:43 +0800

    增加getSomeYourNamePicFromTieba

--- remind/spider2LiaoWithAsync.py
+++ remind/spider2LiaoWithAsync.py
@@ -5,7 +5,7 @@
 # !/usr/bin/env/ python
 import requests
 import time
-from pymongo import MongoClient # mongo处理
+from pymongo import MongoClient  # mongo处理
 from datetime import timedelta
 import re
 from bs4 import BeautifulSoup
@@ -17,31 +17,37 @@ __author__ = 'hc'
 concurrency = 10
 
 headers = {
- # GET http://www.jobbole.com/ HTTP/1.1
- "Host": "www.jobbole.com",
- "Proxy-Connection": "keep-alive",
- "Pragma": "no-cache",
- "Cache-Control": "no-cache",
- "Upgrade-Insecure-Requests": '1',
- "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
- "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
- "Accept-Encoding": "gzip, deflate, sdch",
- "Accept-Language": "zh-CN,zh;q=0.8",
+    # GET http://www.jobbole.com/ HTTP/1.1
+    "Host": "www.jobbole.com",
+    "Proxy-Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
+    "Upgrade-Insecure-Requests": '1',
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, sdch",
+    "Accept-Language": "zh-CN,zh;q=0.8",
 }
+
 
 @gen.coroutine()
 def get_posts_url_from_page(page_url):
- """
- 获取知道页面中所有文章的URL
- :param page_url:
- :return:
- """
- try:
-  response = yield httpclient.AsyncHTTPClient().fetch(page_url, headers = headers)
-  soup = BeautifulSoup(response.body, 'html.parser')
-  posts_tag = soup.find_all('div', class_ = 'post floated-thumb')
-  urls = []
-  for index, archive in enumerate(posts_tag)
+    """
+    获取知道页面中所有文章的URL
+    :param page_url:
+    :return:
+    """
+    try:
+        response = yield httpclient.AsyncHTTPClient().fetch(page_url, headers=headers)
+        soup = BeautifulSoup(response.body, 'html.parser')
+        posts_tag = soup.find_all('div', class_='post floated-thumb')
+        urls = []
+        for index, archive in enumerate(posts_tag):
+            print(index, archive)
+
+    finally:
+        pass
+
 
 response = requests.get("http://python.jobbole.com/87305/")
 print(response.content.decode('utf-8'))
