848438d738facd2eb3b8227fbc0b9658e29f50c2
Administrator <944951213@qq.com>
2017-08-26 10:24:08 +0800

    simpleWebServer不需要，删除之

--- serverOfTornado/7_1_auth.py
+++
@@ -1,7 +0,0 @@
-# Tornado的auth模块 进行外部服务认证
-import tornado.web
-import tornado.httpserver
-import tornado.auth
-import tornado.ioloop
-
-# class TwitterHandler(tornado.web.RequestHandler, tornado.auth.)
--- simpleWebServer/simpleWXPY.py
+++
@@ -1,6 +0,0 @@
-# 登陆微信
-from wxpy import *
-
-bot  = Bot()
-my_friend = bot.friends()
-print(my_friend)
